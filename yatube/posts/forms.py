6636d72ac205951a11a0d4ded8ea3b490842d27e
Анна Андреева <a_n_y_a_s@mail.ru>
2022-08-19 23:03:33 +0300

    Fixed spases to PEP8.

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group',)
-        labels = {'text':'Введите текст', 'group':'Выберите группу'}
+        labels = {'text': 'Введите текст', 'group': 'Выберите группу', }
         help_texts = {
             'text': 'Текст нового поста',
             'group': 'Группа, к которой будет относиться пост',
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -18,7 +18,7 @@ def index(request):
     """Вернуть рендер главной страницы."""
     post_list = Post.objects.select_related('author', 'group').all()
     page_obj = paginate_page(request, post_list)
-    context = {'page_obj': page_obj,}
+    context = {'page_obj': page_obj, }
     return render(request, 'posts/index.html', context)
 
 
@@ -47,7 +47,7 @@ def profile(request, username):
 def post_detail(request, post_id):
     """Вернуть рендер страницы с постом."""
     post = get_object_or_404(Post, id=post_id)
-    context = {'post': post,}
+    context = {'post': post, }
     return render(request, 'posts/post_detail.html', context)
 
 
@@ -56,14 +56,14 @@ def post_create(request):
     """Создать новый пост пользователя."""
     if request.method != "POST":
         form = PostForm()
-        return render(request, 'posts/create_post.html', {'form': form,})
+        return render(request, 'posts/create_post.html', {'form': form, })
     form = PostForm(request.POST)
     if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', post.author)
-    return render(request, 'posts/create_post.html', {'form': form,})
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', post.author)
+    return render(request, 'posts/create_post.html', {'form': form, })
 
 
 @login_required
@@ -80,10 +80,10 @@ def post_edit(request, post_id):
         return render(request, 'posts/create_post.html', context)
     form = PostForm(request.POST, instance=post)
     if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:post_detail', post.id)
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:post_detail', post.id)
     context = {'form': form,
                'is_edit': True
                }
